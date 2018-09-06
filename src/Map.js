65263180be5a212430cdb38c2e3ff3fe6cf47eda
Nouran <only1nour@ymail.com>
2018-09-06 12:43:41 +0200

    Adding markers for locations with popups

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 import mapboxgl from 'mapbox-gl';
 
+// Offsets of the Popups from documentation: https://www.mapbox.com/mapbox-gl-js/api/#popup
+var markerHeight = 50, markerRadius = 10, linearOffset = 25;
+var popupOffsets = {
+    'top': [0, 0],
+    'top-left': [0,0],
+    'top-right': [0,0],
+    'bottom': [0, -markerHeight],
+    'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+    'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+    'left': [markerRadius, (markerHeight - markerRadius) * -1],
+    'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+};
+
 class Map extends Component {
 
     componentDidMount() {
@@ -10,6 +23,23 @@ class Map extends Component {
             style: 'mapbox://styles/mapbox/streets-v9',
             center: [31.200092, 28.318739],
             zoom: 5.5
+        });
+
+        this.createMarkersWithPopups(map);
+    }
+
+    // Creates the markers specific for each location with corresponding popup
+    createMarkersWithPopups(map) {
+        const locations = this.props.mapLocations;
+
+        locations.forEach(location => {
+            var popup = new mapboxgl.Popup({offset: popupOffsets, className: 'pop-up'})
+                .setText(location.name);
+        
+            var marker = new mapboxgl.Marker()
+                .setLngLat(location.latLng)
+                .setPopup(popup)
+                .addTo(map);
         });
     }
 
